badb6a1d4b0998c2b7f679e5b096466e77632530
Gyuseok Lee <hahohh@gmail.com>
2021-03-29 21:44:39 +0900

    Update front-api-msa.go

--- front-api-msa.go
+++ front-api-msa.go
@@ -1,56 +1,36 @@
-job "05-api-go" {
-  datacenters = ["hashistack"]
-  namespace = "msa"
+package main
 
-  type = "service"
+import (
+    "net/http"
+    "github.com/labstack/echo"
+    b64 "encoding/base64"
+    "bytes"
+    "fmt"
+)
 
-  group "front" {
-    count = 1
-    task "go" {
-      driver = "raw_exec"
-      env {
-        GOPATH = "${NOMAD_TASK_DIR}/workspace"
-        GOCACHE = "${NOMAD_TASK_DIR}/cache"
-        APPPATH = "${NOMAD_TASK_DIR}/workspace/src/hello"
-      }
-      template {
-data = <<EOF
-#!/bin/sh
-echo "***** start EVN *****"
-mkdir -p ${APPPATH}
-mkdir ./cache
-cp ./local/front-api.go ${APPPATH}/
-cd ${APPPATH}
-echo "***** start go ****"
-/usr/bin/go get
-/usr/bin/go run front-api.go
-EOF
-        destination = "local/start.sh"
-      }
-      config {
-        command = "local/start.sh"
-      }
-      artifact {
-        source = "https://raw.githubusercontent.com/Great-Stone/various-workloads-on-nomad/master/front-api.go"
-      }
-      resources {
-        network {
-          port "http" {
-            static = 8888
-          }
-        }
-      }
-      service {
-        name = "msa-front-go"
-        tags = ["go", "api"]
+func main() {
+    e := echo.New()
+    e.GET("/animal/:name", invokeFunc)
+    e.Logger.Fatal(e.Start(":8888"))
+}
 
-        check {
-          type  = "tcp"
-          interval = "10s"
-          timeout  = "2s"
-          port  = "http"
-        }
-      }
+func invokeFunc(c echo.Context) error{
+    animal := c.Param("name")
+    fmt.Println(animal)
+	animal64 := b64.StdEncoding.EncodeToString([]byte(animal))
+	
+	var jsonStr = []byte(`{"Payload":"` + animal64 + `"}`)
+    url := "http://172.28.128.31:4646/v1/job/04-parameterized-toUpper/dispatch"
+    req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+    req.Header.Set("Content-Type", "application/json")
+    
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
     }
-  }
+    defer resp.Body.Close()
+
+    return err
 }
